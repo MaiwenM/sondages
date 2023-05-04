5a0daeb4e4ac2533f513d8969ade779d3b29f1d1
adermont <adermont.github@gmail.com>
2023-05-04 14:10:18 +0200

    Ajout des pages listUsers updateUser

--- src/main/java/fr/simplon/sondages/web/UsersController.java
+++ src/main/java/fr/simplon/sondages/web/UsersController.java
@@ -7,7 +7,10 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,20 +23,22 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.sql.DataSource;
 import java.security.Principal;
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.*;
 
 @Controller
 public class UsersController
 {
+    private DataSource         dataSource;
     private PasswordEncoder    passwordEncoder;
     private UserDetailsManager userDetailsManager;
 
     @Autowired
     public UsersController(
-            PasswordEncoder pPasswordEncoder, UserDetailsManager pUserDetailsManager)
+            DataSource pDataSource, PasswordEncoder pPasswordEncoder, UserDetailsManager pUserDetailsManager)
     {
+        dataSource = pDataSource;
         passwordEncoder = pPasswordEncoder;
         userDetailsManager = pUserDetailsManager;
     }
@@ -82,7 +87,7 @@ public class UsersController
 
         // Create the account in database with all its roles
         userDetailsManager.createUser(userDetails);
-        return "redirect:/login";
+        return "redirect:/admin/listUsers";
     }
 
     @GetMapping("/inscription")
@@ -93,10 +98,73 @@ public class UsersController
     }
 
     @PostMapping("/inscription")
-    public String subscribe(@ModelAttribute(name = "user") @Valid CreateUserForm user, BindingResult validation, Model model)
+    public String subscribe(
+            @ModelAttribute(name = "user") @Valid CreateUserForm user, BindingResult validation, Model model)
     {
-
         return createUser(user, validation, model);
+    }
+
+    @GetMapping("/admin/listUsers")
+    public String getUsers(Model model)
+    {
+        if (!model.containsAttribute("users"))
+        {
+            RowMapper<UserDetails> mapper = (rs, rowNum) -> {
+                String username1 = rs.getString(1);
+                String password = rs.getString(2);
+                boolean enabled = rs.getBoolean(3);
+                return new User(username1, password, Collections.emptyList());
+            };
+
+            JdbcTemplate jdbc = new JdbcTemplate(dataSource);
+            List<UserDetails> users = jdbc.query("select username,password,enabled from users", mapper);
+            model.addAttribute("users", users);
+        }
+        return "/admin/listUsers";
+    }
+
+    @GetMapping("/admin/updateUser/{userId}")
+    public String getUpdateUserForm(@PathVariable String userId, Model model)
+    {
+        if (!model.containsAttribute("user"))
+        {
+            CreateUserForm userForm = new CreateUserForm();
+            userForm.setLogin(userId);
+            model.addAttribute("user", userForm);
+        }
+        return "/admin/updateUser";
+    }
+
+    @PostMapping("/admin/updateUser/{userId}")
+    @Transactional
+    public String updateUser(
+            @PathVariable String userId,
+            @Valid @ModelAttribute(name = "user") CreateUserForm userForm,
+            BindingResult validation,
+            Model model)
+    {
+        // Recherche de l'utilisateur en BDD
+        Optional<String> login = Optional.ofNullable(userForm.getLogin());
+        UserDetails userDetails = login.map(l -> userDetailsManager.loadUserByUsername(l)).orElse(null) ;
+        if (userDetails == null)
+        {
+            validation.addError(new FieldError("user", "username", "Utilisateur inconnu"));
+        }
+
+        // Vérification des 2 mots de passe fournis, ils doivent être égaux
+        if (!validation.hasErrors() && !Objects.equals(userForm.getPassword(), userForm.getConfirmPassword()))
+        {
+            validation.addError(new FieldError("user", "confirmPassword", "Les mots de passe ne correspondent pas"));
+        }
+
+        if (!validation.hasErrors())
+        {
+            User.UserBuilder builder = User.withUserDetails(userDetails);
+            builder.password(userDetails.getPassword());
+            userDetailsManager.updateUser(builder.build());
+            model.addAttribute("success", Boolean.TRUE);
+        }
+        return "/admin/updateUser";
     }
 
     @GetMapping("/change-password")
